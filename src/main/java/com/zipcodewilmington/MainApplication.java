ed36cf3dc632755424588808844e52ed8a4c0646
L. Dolio Durant <dolio@zipcodewilmington.com>
2024-12-06 09:51:24 -0500

    adding starting point for lab

---
+++ src/main/java/com/zipcodewilmington/MainApplication.java
@@ -0,0 +1,15 @@
+package com.zipcodewilmington;
+
+/**
+ * Main Application
+ */
+public class MainApplication {
+    public static void main(String[] argv) {
+//        System.out.println(System.getenv("JAVA_HOME"));
+//        System.out.println(System.getenv("PLAYWITH_DB_USERNAME"));
+//        System.out.println(System.getenv("PLAYWITH_DB_PASSWORD"));
+//        System.out.println(System.getenv("PLAYWITH_DB_HOST"));
+//        System.out.println(System.getenv("PLAYWITH_DB_PORT"));
+//        System.out.println(System.getenv("PLAYWITH_DB"));
+    }
+}
